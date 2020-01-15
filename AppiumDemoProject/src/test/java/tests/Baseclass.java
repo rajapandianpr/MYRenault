83cc92edd84dab2e9e728b98261e6c64e018103d
amsaleka <amsaleka@Tti-LP-021.tti-global.com>
2020-01-15 21:06:23 +0530

    MYRRenault-My Vehicle(s)

--- AppiumDemoProject/src/test/java/tests/Baseclass.java
+++ AppiumDemoProject/src/test/java/tests/Baseclass.java
@@ -35,6 +35,8 @@ public class Baseclass {
 	  caps.setCapability(MobileCapabilityType.PLATFORM_NAME,"Android");
 	  caps.setCapability(MobileCapabilityType.PLATFORM_VERSION,"9");
 	  caps.setCapability(MobileCapabilityType.DEVICE_NAME,"PL2GAMG870202870");
+	  caps.setCapability("realMobile", "true");
+	  caps.setCapability("autoGrantPermissions", "true");
 	 // caps.setCapability(MobileCapabilityType.UDID,"emulator-5554");
 	  caps.setCapability(MobileCapabilityType.APP,"C:\\Users\\amsaleka\\apps\\reno.apk");
 	  
@@ -52,10 +54,143 @@ public class Baseclass {
 	  
 	  driver.manage().timeouts().implicitlyWait(60,TimeUnit.SECONDS);
 		 
-	 driver.findElement(By.className("//android.widget.Button[@text='RENAULT OWNER'and @index='0']")).click();
-	  
-	                                                                                                                                                                                                                                                                                                                                                                                                     
+	
+	           MobileElement two = driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View/android.view.View[1]/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.view.View[2]/android.view.View/android.widget.Button[1]\r\n" + 
+	           		""));    
+	           two.click();  // Renault Owner
+	           
+	           
+	           
+	     	  driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
+	     	  
 
+	           MobileElement three = driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View/android.view.View[1]/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.view.View[3]/android.widget.Button[2]\r\n" + 
+	           		""));
+	           three.click(); // Sign in
+	           
+	           driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
+	          driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View/android.view.View[1]/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.view.View[2]/android.view.View/android.view.View/android.view.View[2]/android.view.View[2]/android.view.View/android.view.View[2]/android.widget.EditText\r\n" + 
+	          		"")).sendKeys("9841676067"); //Login
+	          
+	          driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
+	          
+	          driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View/android.view.View[1]/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.view.View[2]/android.view.View/android.view.View/android.view.View[3]/android.view.View[2]/android.view.View/android.view.View[2]/android.view.View\r\n" + 
+	          		"\r\n" + 
+	          		
+		          	"")).sendKeys("dhina12"); //password
+	          
+	     	  driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
+	     	 MobileElement four = driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View/android.view.View[1]/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.view.View[2]/android.view.View/android.view.View/android.widget.Button\r\n" + 
+	     	 		""));
+		           four.click(); // Sign in
+		             
+				     	 driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
+				     	
+		 driver.context("NATIVE_APP");
+				  
+		           driver.findElement(By.xpath(".//android.widget.Button[@text='Ok, Got it']")).click();
+		           Thread.sleep(5000);	
+		           driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
+		                    
+		  	MobileElement six = driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View/android.view.View[1]/android.view.View/android.view.View/android.view.View[2]/android.view.View[2]/android.view.View[3]/android.view.View/android.view.View[1]/android.view.View[2]"));
+		  			        six.click(); // My vehicle
+						           
+	driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
+	driver.context("NATIVE_APP");
+    driver.findElement(By.xpath(".//android.widget.Button[@text='Ok, Got it']")).click();
+    Thread.sleep(5000);	
+    driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
+	MobileElement seven = driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View/android.view.View[1]/android.view.View/android.view.View/android.view.View[2]/android.view.View[2]/android.view.View/android.view.View/android.view.View/android.view.View[6]/android.view.View/android.view.View[1]/android.view.View\r\n" + 
+			""));
+      seven.click(); // My Vehicle Details 
+      driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
+  	MobileElement eight = driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View/android.view.View[1]/android.view.View/android.view.View/android.view.View[2]/android.view.View[2]/android.view.View/android.view.View/android.view.View/android.view.View[6]/android.view.View/android.view.View[1]/android.view.View\r\n" + 
+  			""));
+        eight.click(); // My Vehicle Details - double tap
+        
+        driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);  
+        
+        MobileElement nine = driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View/android.view.View[1]/android.view.View/android.view.View/android.view.View[2]/android.view.View[2]/android.view.View/android.view.View/android.view.View/android.view.View[7]/android.view.View/android.view.View[1]/android.view.View\r\n"));
+        	
+            nine.click(); // My Maintenance Schedule
+            
+            driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
+            
+            MobileElement ten = driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View/android.view.View[1]/android.view.View/android.view.View/android.view.View[2]/android.view.View[2]/android.view.View/android.view.View/android.view.View/android.view.View[7]/android.view.View[10]/android.widget.Button\r\n"));	
+                ten.click(); // Maintenance Plan - click here 
+                
+                driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
+                
+                MobileElement eleven = driver.findElement(By.xpath("//android.widget.ImageButton[@content-desc=\"Close Button\"]"));
+                eleven.click(); // PDF Close 
+                
+                driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
+                
+                MobileElement tweleve = driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View/android.view.View[1]/android.view.View/android.view.View/android.view.View[2]/android.view.View[2]/android.view.View/android.view.View/android.view.View/android.view.View[8]/android.view.View/android.view.View[1]/android.view.View\r\n" + 
+                		""));
+                tweleve.click(); // My Warranties Schedule
+                
+                driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
+    
+                MobileElement thrityteen = driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View/android.view.View[1]/android.view.View/android.view.View/android.view.View[2]/android.view.View[2]/android.view.View/android.view.View/android.view.View/android.view.View[8]/android.view.View/android.view.View[1]/android.view.View\\r\\n"));
+                thrityteen.click(); // My Warranties Schedule - double tab
+                
+                driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
+                
+                MobileElement fourteen = driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View/android.view.View[1]/android.view.View/android.view.View/android.view.View[2]/android.view.View[2]/android.view.View/android.view.View/android.view.View/android.view.View[9]/android.view.View/android.view.View[1]/android.view.View\r\n" + 
+                		""));
+                fourteen.click(); // Renault Finance
+                
+                driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
+                
+                MobileElement fifteen = driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View/android.view.View[1]/android.view.View/android.view.View/android.view.View[1]/android.view.View/android.widget.Button\r\n" + 
+                		""));
+                fifteen.click(); // Renault Finance back arrow
+                
+               driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
+                
+                MobileElement sixteen = driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View/android.view.View[1]/android.view.View/android.view.View/android.view.View[2]/android.view.View[2]/android.view.View/android.view.View/android.view.View/android.view.View[10]/android.view.View/android.view.View[1]/android.view.View\r\n" + 
+                		""));
+                sixteen.click(); // Additional information 
+                
+            driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
+                
+                MobileElement seventeen = driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View/android.view.View[1]/android.view.View/android.view.View/android.view.View[2]/android.view.View[2]/android.view.View/android.view.View/android.view.View/android.view.View[10]/android.view.View/android.view.View[1]/android.view.View\\r\\n"));
+                seventeen .click(); // Additional information
+                
+          driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
+                
+                MobileElement eighteen = driver.findElement(By.xpath(""));
+                eighteen.click(); // 
+                
+          driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
+                
+           /*     MobileElement nineteen = driver.findElement(By.xpath(""));
+                nineteen.click(); //  
+ driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
+                
+                MobileElement twenty = driver.findElement(By.xpath(""));
+                twenty.click(); // 
+                */
+                
+                
+                
+                
+
+
+                
+
+              
+                
+                
+            
+            
+            
+        
+             
+      
+           
+       
   }
    
   catch(Exception exp) {
